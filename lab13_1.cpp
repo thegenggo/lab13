2a6112df0b2a8dff47d0a8ae7a1c834261e7c504
thegenggo <kritsanaphong-keang@hotmail.com>
2021-01-22 16:04:37 +0700

    lab13_1.cpp Update

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,18 +2,44 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[], int N)
+{
+	for (int i = 1; i < N; i++)
+	{
+		T temp = d[i];
+		if (temp > d[i - 1])
+		{
+			for (int j = i - 1; j >= 0; j--)
+			{
 
+				d[j + 1] = d[j];
+				if (temp <= d[j - 1] || j == 0)
+				{
+					d[j] = temp;
+					break;
+				}
+			}
+		}
+		cout << "Pass " << i << ":";
+		for (int k = 0; k < N; k++)
+		{
+			cout << d[k] << " ";
+		}
+		cout << "\n";
+	}
 }
 
-int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+int main()
+{
+	double a[10] = {69.9,0.25,0.5,1.34,2.1,0,4.8,88.36,11.01,41.25};
 	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
 	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
+
+	insertionSort(a, 10);
+
 	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
 }
